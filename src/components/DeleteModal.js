b1f51a1a654512dc4d28e973e448fbf341956acc
Abdul-Wadud Sumaila <abdul.sumaila@amalitech.org>
2022-02-07 22:44:33 +0000

    return funtion initial commit

--- src/components/DeleteModal.js
+++ src/components/DeleteModal.js
@@ -35,4 +35,33 @@ export const deleteModal = (id, setDeleteComment, data, setData) => {
         }
       }; 
 
+      return (
+        <div className="deleteModal">
+          <div className="deleteModalBox">
+            <h2 className="deleteModalTitle">Delete comment</h2>
+            <p className="deleteModalText">
+              Are you sure you want to delete this comment? This will remove the
+              comment and can't be undone.
+            </p>
+            <div className="deleteModalButtonsContainer">
+              <button
+                className="deleteModalButton cancelDeleteButton mouse-pointer"
+                onClick={() => setDeleteComment(false)}
+              >
+                NO, CANCEL
+              </button>
+              <button
+                className="deleteModalButton confirmDeleteButton mouse-pointer"
+                onClick={() => {
+                  deleteComment(deleteComment.id);
+                  setDeleteComment(false);
+                }}
+              >
+                YES, DELETE
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+
 }
