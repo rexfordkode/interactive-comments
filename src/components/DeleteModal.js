a4a8582477c9f2c9c74e08d91af368ffdbb03efb
Abdul-Wadud Sumaila <abdul.sumaila@amalitech.org>
2022-02-07 22:28:09 +0000

    DeleteModal updates

--- src/components/DeleteModal.js
+++ src/components/DeleteModal.js
@@ -1,17 +1,38 @@
 import './deleteModal.css';
 
 export const deleteModal = (id, setDeleteComment, data, setData) => {
-
-    const deleteComment = () => {
+    
+    // function for deleting user comment
+    const deleteComment = (id) => {
         let temp = data;
-        for (let comment of temp.comments){
-            if (comment.id === id) {
-                const updatedComments = temp.comments.filter(
-                    (comment) => comment.id !== id
-                );
-        } 
-    }
-
-   }
+        for (let comment of temp.comments) {
+          if (comment.id === id) {
+            temp.comments = temp.comments.filter(
+              (filComment) => filComment.id !== id
+            );
+            break;
+          } else {
+            if (comment?.replies?.length > 0) {
+              findCommentToDelete(comment, comment.replies, id);
+            }
+          }
+        }
+        setData({ ...temp });
+      };
+      const findCommentToDelete = (parent, replies, id) => {
+        let temp = parent;
+        for (let reply of replies) {
+          if (reply.id === id) {
+            temp.replies = temp.replies.filter(
+              (filComment) => filComment.id !== id
+            );
+            break;
+          } else {
+            if (reply?.replies?.length > 0) {
+              findCommentToDelete(reply, reply.replies, id);
+            }
+          }
+        }
+      }; 
 
 }
