e3785bc55ba087c38b92336b1f9eb154a3b3efec
rexfordkode <86994591+rexfordkode@users.noreply.github.com>
2022-02-08 00:48:29 +0000

    Update on components App.js

---
+++ src/components/newReply.js
@@ -0,0 +1,38 @@
+import './newComment.css'
+let reply;
+
+export const NewReply = (
+    {
+        setNewReply,
+        currentUser,
+        addNewReply,
+        parentId
+    }
+) =>{
+    return (
+        <div className='newComment'>
+            <div className='avatarColumn'>
+                <img className='avatarReply' src={currentUser.image.png}  alt='curent user'/>
+            </div>
+            <div className='inputComment'>
+                <textarea 
+                className='replyInput'
+                placeholder='Add a comment..'
+                onChange={(e) => {reply = e.target.value}}
+                >
+                </textarea>
+            </div>
+
+        <div className='sendColumn'>
+            <button className='sendButton' onClick={(e) =>{
+                addNewReply(parentId, reply)
+                setNewReply(false)
+            }} >
+                REPLY
+            </button>
+
+            </div>
+        </div>
+    )
+
+}
