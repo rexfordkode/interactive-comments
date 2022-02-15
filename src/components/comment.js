596a7a05c20893973fe73c1a4c99ab57b9fcd0f4
Abdul-Wadud Sumaila <abdul.sumaila@amalitech.org>
2022-02-15 08:51:51 +0000

    conflicts resolve

--- src/components/comment.js
+++ src/components/comment.js
@@ -2,11 +2,7 @@ import { useEffect, useState } from "react";
 import NewReply from "./newReply";
 import "./comment.css";
 
-<<<<<<< HEAD
 export default function Comment({
-=======
-const Comment = ({
->>>>>>> 618263333564604fc89367e44263a26f53abe9bc
   id,
   currentUser,
   replyingTo,
@@ -20,20 +16,9 @@ const Comment = ({
   updateComment,
   setDeleteComment,
   addNewReply,
-<<<<<<< HEAD
 }) {
   const [newReply, setNewReply] = useState(false);
   const [vote, setVote] = useState(false);
-=======
-}) => {
-  const [newReply, setNewReply] = useState(false);
-  //   modified
-  const [vote, setVote] = useState(false);
-
-  const [iconPlusFill, setIconPlusFill] = useState("#c5c6ef");
-  const [iconMinusFill, setIconMinusFill] = useState("#c5c6ef");
-
->>>>>>> 618263333564604fc89367e44263a26f53abe9bc
   const [edit, setEdit] = useState(false);
   const [current, setCurrent] = useState(false);
 
@@ -43,18 +28,12 @@ const Comment = ({
     setCurrent(curr);
   }, [currentUser, username]);
 
-<<<<<<< HEAD
-=======
-  //
-
->>>>>>> 618263333564604fc89367e44263a26f53abe9bc
   return (
     <>
       {
         <div className="comment">
           <div className="scoreColumn">
             {
-<<<<<<< HEAD
               // disable voting function for user's own comments
               current ? (
                 <>
@@ -95,83 +74,6 @@ const Comment = ({
                       }
                     }}
                   />
-=======
-              // Disable voting function for user's own comments
-              current ? (
-                <>
-                  <div className="flex-item upvote ">
-                    <svg
-                      width="11"
-                      height="11"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
-                        fill="#C5C6EF"
-                      />
-                    </svg>
-                  </div>
-
-                  <span className="flex-item">{score}</span>
-                  <div className="flex-item updown">
-                    <svg
-                      width="11"
-                      height="3"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"
-                        fill="#C5C6EF"
-                      />
-                    </svg>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <div className="flex-item upvote viewervote">
-                    <svg
-                      width="11"
-                      height="11"
-                      xmlns="http://www.w3.org/2000/svg"
-                      onClick={() => {
-                        if (!vote) {
-                          updateScore(id, "upvote");
-                          setVote(true);
-                          setIconPlusFill("#C5C6EF");
-                          setIconMinusFill("#C5C6EF");
-                        }
-                      }}
-                    >
-                      <path
-                        d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
-                        fill={iconPlusFill}
-                      />
-                    </svg>
-                  </div>
-
-                  <span className="flex-item">{score}</span>
-
-                  <div className="flex-item updown viewervote">
-                    <svg
-                      width="11"
-                      height="3"
-                      xmlns="http://www.w3.org/2000/svg"
-                      onClick={() => {
-                        if (!vote) {
-                          updateScore(id, "downvote");
-                          setVote(true);
-                          setIconMinusFill("#C5C6EF");
-                          setIconPlusFill("#C5C6EF");
-                        }
-                      }}
-                    >
-                      <path
-                        d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"
-                        fill={iconMinusFill}
-                      />
-                    </svg>
-                  </div>
->>>>>>> 618263333564604fc89367e44263a26f53abe9bc
                 </>
               )
             }
@@ -260,13 +162,9 @@ const Comment = ({
                 {comment}
               </div>
             )}
-<<<<<<< HEAD
           </div>{" "}
           {/* contentColumn*/}
           {/* comment*/}
-=======
-          </div>
->>>>>>> 618263333564604fc89367e44263a26f53abe9bc
         </div>
       }
       {newReply !== false && (
@@ -304,10 +202,4 @@ const Comment = ({
         })}
     </>
   );
-<<<<<<< HEAD
 }
-=======
-};
-
-export default Comment;
->>>>>>> 618263333564604fc89367e44263a26f53abe9bc
